0d918ed8cbf0620815606bd06faed4700961e2dc
Jeffrey Kosse <j3fftw@gmail.com>
2022-03-06 22:48:21 +0100

    work on fix

--- src/main/java/me/profelements/dynatech/items/tools/LiquidTank.java
+++ src/main/java/me/profelements/dynatech/items/tools/LiquidTank.java
@@ -12,21 +12,18 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.collections.Pair;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.data.persistent.PersistentDataAPI;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction;
-import io.github.thebusybiscuit.slimefun4.libraries.paperlib.PaperLib;
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 import me.profelements.dynatech.DynaTech;
 import me.profelements.dynatech.DynaTechItems;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
+import org.bukkit.block.data.Levelled;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,7 +34,8 @@ public class LiquidTank extends SlimefunItem implements NotPlaceable {
 
     private final int maxLiquidAmount;
 
-    public LiquidTank(ItemGroup itemGroup, SlimefunItemStack item, int maxLiquidAmount, RecipeType recipeType, ItemStack[] recipe) {
+    public LiquidTank(ItemGroup itemGroup, SlimefunItemStack item, int maxLiquidAmount, RecipeType recipeType,
+                      ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
 
         this.maxLiquidAmount = maxLiquidAmount;
@@ -46,51 +44,68 @@ public class LiquidTank extends SlimefunItem implements NotPlaceable {
         addItemHandler(onEntityClick());
     }
 
-    private final EntityInteractHandler onEntityClick() {
+    private EntityInteractHandler onEntityClick() {
         return (e, item, something) -> {
-            if ((e.getRightClicked().getType() == EntityType.COW || e.getRightClicked().getType() == EntityType.MUSHROOM_COW) && SlimefunUtils.isItemSimilar(item, DynaTechItems.LIQUID_TANK, true)) {
+            if ((e.getRightClicked().getType() == EntityType.COW
+                || e.getRightClicked().getType() == EntityType.MUSHROOM_COW)
+                && SlimefunUtils.isItemSimilar(item, DynaTechItems.LIQUID_TANK, true)
+            ) {
                 e.setCancelled(true);
             }
         };
     }
 
-    private final ItemUseHandler onRightClick() {
+    private ItemUseHandler onRightClick() {
         return e -> {
             e.cancel();
 
-            Optional<Block> b = e.getClickedBlock();
+
+
+            Block b = e.getPlayer().getTargetBlock(null, 3);
+
             Optional<SlimefunItem> item = e.getSlimefunItem();
             ItemStack itemStack = e.getItem();
-            
-            if (b.isPresent() && item.isPresent() && item.get() instanceof LiquidTank && Slimefun.getProtectionManager().hasPermission(e.getPlayer(), b.get().getLocation(), Interaction.PLACE_BLOCK)) {
-                Block liquid = b.get().getRelative(e.getClickedFace());
-                BlockState liquidState = PaperLib.getBlockState(liquid, false).getState();
-                LiquidTank liquidTank = (LiquidTank) item.get();
 
+            if (item.isPresent()
+                && item.get() instanceof LiquidTank
+                && Slimefun.getProtectionManager().hasPermission(e.getPlayer(), b,
+                Interaction.PLACE_BLOCK)
+            ) {
+                Block liquid = b.getRelative(e.getClickedFace());
+                LiquidTank liquidTank = (LiquidTank) item.get();
                 String fluidName = getLiquid(itemStack).getFirstValue();
                 int fluidAmount = getLiquid(itemStack).getSecondValue();
 
-                if (liquid.getType() == Material.AIR && fluidName != null && e.getPlayer().isSneaking() && fluidAmount >= 1000) {
-                        if (fluidName.equals("WATER")) {
-                            removeLiquid(itemStack, fluidName, 1000);
-                            liquidState.setType(Material.WATER);
-                            liquidState.update(true, true);
-                            
-                            updateLore(itemStack);
-                            
-                        } else if (fluidName.equals("LAVA")) {
-                            removeLiquid(itemStack, fluidName, 1000);
-                            liquidState.setType(Material.LAVA);
-                            liquidState.update(true, true);
-                            
-                            updateLore(itemStack);
-                        }
-                    
-                } else if ((liquid.getType() == Material.LAVA || liquid.getType() == Material.WATER) && fluidName != null && fluidAmount <= liquidTank.getMaxLiquidAmount() && liquid.isLiquid()) {
-                        addLiquid(itemStack, liquid.getType().name(), 1000);
-                        liquidState.setType(Material.AIR);
-                        liquidState.update(true, true);
+                if (liquid.getType() == Material.AIR
+                    && fluidName != null
+                    && e.getPlayer().isSneaking()
+                    && fluidAmount >= 1000
+                ) {
+                    if (fluidName.equals("WATER")) {
+                        removeLiquid(itemStack, fluidName, 1000);
+                        liquid.setType(Material.WATER, true);
                         updateLore(itemStack);
+
+                    } else if (fluidName.equals("LAVA")) {
+                        removeLiquid(itemStack, fluidName, 1000);
+                        liquid.setType(Material.LAVA, true);
+                        updateLore(itemStack);
+                    }
+
+                } else if ((liquid.getType() == Material.LAVA
+                    || liquid.getType() == Material.WATER)
+                    && fluidName != null
+                    && fluidAmount <= liquidTank.getMaxLiquidAmount()
+                    && liquid.isLiquid()
+                ) {
+
+                    Levelled liquidData = (Levelled) liquid.getBlockData();
+
+                    if (liquidData.getLevel() == 0) {
+                        addLiquid(itemStack, liquid.getType().name(), 1000);
+                        liquid.setType(Material.AIR, true);
+                        updateLore(itemStack);
+                    }
                 }
             }
         };
@@ -98,14 +113,6 @@ public class LiquidTank extends SlimefunItem implements NotPlaceable {
 
     public int getMaxLiquidAmount() {
         return maxLiquidAmount;
-    }
-
-    public static final List<String> getPlaceableFluids() {
-        List<String> PLACEABLE_FLUIDS = new ArrayList<>();
-        PLACEABLE_FLUIDS.add("WATER");
-        PLACEABLE_FLUIDS.add("LAVA");
-
-        return PLACEABLE_FLUIDS;
     }
 
     public void addLiquid(ItemStack item, String fluidName, int fluidAmount) {
@@ -132,7 +139,11 @@ public class LiquidTank extends SlimefunItem implements NotPlaceable {
         int itemFluidAmount = PersistentDataAPI.getInt(im, FLUID_AMOUNT);
 
         int resultFluidAmount = itemFluidAmount - fluidAmount;
-        if (itemFluidName != null && itemFluidName.equals(fluidName) && itemFluidAmount != 0 && resultFluidAmount > 0) {
+        if (itemFluidName != null
+            && itemFluidName.equals(fluidName)
+            && itemFluidAmount != 0
+            && resultFluidAmount > 0
+        ) {
             setLiquid(item, fluidName, resultFluidAmount);
         } else {
             setLiquid(item, "NO_FLUID", 0);
